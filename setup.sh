7892db1bb4f6eb781ec11dea8b27b3ed2e95de5b
Brandon <willjasen@gmail.com>
2016-04-16 23:36:22 -0400

    enable IPv4 forwarding (not done)

--- setup.sh
+++ setup.sh
@@ -6,11 +6,13 @@ sudo apt-get -y update
 # Install OpenVPN
 sudo apt-get -y install openvpn
 
-# Gather ProXPN user credentials
+# Gather ProXPN user credentials and save
 echo -n "Enter ProXPN username: "
 read proxpn_username
 echo -n "Enter ProXPN password: "
 read proxpn_password
-
 echo $proxpn_username >> /etc/openvpn/creds.conf
 echo $proxpn_password >> /etc/openvpn/creds.conf
+
+# Enable IPv4 forwarding
+echo '1' | sudo tee /proc/sys/net/ipv4/ip_forward
