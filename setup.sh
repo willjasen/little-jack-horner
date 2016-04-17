d1409f37b9fbf27a5f8c0d2a91f1144a4a6d80fc
Brandon <willjasen@gmail.com>
2016-04-16 23:42:30 -0400

    finished enabling IPv4 forwarding

--- setup.sh
+++ setup.sh
@@ -16,3 +16,4 @@ echo $proxpn_password >> /etc/openvpn/creds.conf
 
 # Enable IPv4 forwarding
 echo '1' | sudo tee /proc/sys/net/ipv4/ip_forward
+sudo sed -i '/net.ipv4.ip_forward/s/^#//g' /etc/sysctl.conf
