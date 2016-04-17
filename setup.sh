5b4c3ac8c8554a38c2c445fc577fb987c0c2e674
Brandon <willjasen@gmail.com>
2016-04-16 23:03:10 -0400

    gather ProXPN credentials

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ sudo apt-get -y update
 
 # Install OpenVPN
 sudo apt-get -y install openvpn
+
+# Gather ProXPN user credentials
+echo -n "Enter ProXPN username: "
+read proxpn_username
+echo -n "Enter ProXPN password: "
+read proxpn_password
