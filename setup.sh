31b3930b83158e57b1bb3e9598db3444adb914ab
willjasen <willjasen@gmail.com>
2016-05-11 19:58:49 -0400

    save iptables rules

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ sudo sed -i '/net.ipv4.ip_forward/s/^#//g' /etc/sysctl.conf
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 
 # Save iptables rule
-sudo iptables-save > /etc/iptables.up.rules
+sudo iptables-save | sudo tee -a /etc/iptables.up.rules
 echo 'iptables-restore < /etc/iptables.up.rules' | sudo tee -a /etc/network/if-pre-up.d/iptables
 echo 'exit 0' | sudo tee -a /etc/network/if-pre-up.d/iptables
 sudo chown root:root /etc/network/if-pre-up.d/iptables
